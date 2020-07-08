4df46dc2173731833e3119e958b89c936bf55d0e
jsgazsi <jsgazsi@gmail.com>
2020-07-08 12:22:49 -0400

    Data Update

--- Macro_CSV_Lists.py
+++ Macro_CSV_Lists.py
@@ -12,7 +12,32 @@ US = [
     "https://www.mql5.com/en/economic-calendar/united-states/michigan-consumer-expectations/export",
     "https://www.mql5.com/en/economic-calendar/united-states/michigan-consumer-sentiment/export",
     "https://www.mql5.com/en/economic-calendar/united-states/building-permits/export",
+    #Single Use Indicators
     "https://www.mql5.com/en/economic-calendar/united-states/fed-interest-rate-decision/export",
+    #NEWEST ADDED
+    "https://www.mql5.com/en/economic-calendar/united-states/gross-domestic-product-qq/export",
+    "https://www.mql5.com/en/economic-calendar/united-states/adp-nonfarm-employment-change/export",
+    "https://www.mql5.com/en/economic-calendar/united-states/u6-unemployment-rate/export", #INVERSE!!
+    "https://www.mql5.com/en/economic-calendar/united-states/consumer-price-index-ex-food-energy-yy/export",
+    "https://www.mql5.com/en/economic-calendar/united-states/consumer-price-index-yy/export",
+    "https://www.mql5.com/en/economic-calendar/united-states/pce-price-index-yy/export",
+    "https://www.mql5.com/en/economic-calendar/united-states/producer-price-index-yy/export",
+    "https://www.mql5.com/en/economic-calendar/united-states/dallas-fed-manufacturing-business-index/export",
+    "https://www.mql5.com/en/economic-calendar/united-states/dallas-fed-services-business-activity/export",
+    "https://www.mql5.com/en/economic-calendar/united-states/capacity-utilization/export",
+    "https://www.mql5.com/en/economic-calendar/united-states/ism-manufacturing-employment/export",
+    "https://www.mql5.com/en/economic-calendar/united-states/ism-manufacturing-new-orders/export",
+    "https://www.mql5.com/en/economic-calendar/united-states/ism-non-manufacturing-business-activity/export",
+    "https://www.mql5.com/en/economic-calendar/united-states/ism-non-manufacturing-employment/export",
+    "https://www.mql5.com/en/economic-calendar/united-states/ism-non-manufacturing-new-orders/export",
+    "https://www.mql5.com/en/economic-calendar/united-states/nfib-small-business-optimism/export",
+    "https://www.mql5.com/en/economic-calendar/united-states/michigan-current-conditions/export",
+    "https://www.mql5.com/en/economic-calendar/united-states/retail-sales-yy/export",
+    "https://www.mql5.com/en/economic-calendar/united-states/existing-home-sales/export",
+    "https://www.mql5.com/en/economic-calendar/united-states/housing-starts/export",
+    "https://www.mql5.com/en/economic-calendar/united-states/nahb-housing-market-index/export",
+    "https://www.mql5.com/en/economic-calendar/united-states/new-home-sales/export",
+    "https://www.mql5.com/en/economic-calendar/united-states/pending-home-sales-index/export",
 ]
 
 #Euro Zone
@@ -30,8 +55,19 @@ EU = [
     "https://www.mql5.com/en/economic-calendar/european-union/retail-sales-yy/export",
     "https://www.mql5.com/en/economic-calendar/european-union/construction-output-yy/export",
     "https://www.mql5.com/en/economic-calendar/european-union/industrial-confidence-indicator/export",
+    #Single Use Indicators
     "https://www.mql5.com/en/economic-calendar/european-union/ecb-interest-rate-decision/export",
     "https://www.mql5.com/en/economic-calendar/european-union/cftc-eur-non-commercial-net-positions/export",
+    #NEWEST ADDED
+    "https://www.mql5.com/en/economic-calendar/european-union/gross-domestic-product-yy/export",
+    "https://www.mql5.com/en/economic-calendar/european-union/employment-change-qq/export",
+    "https://www.mql5.com/en/economic-calendar/european-union/employment-change-yy/export",
+    "https://www.mql5.com/en/economic-calendar/european-union/consumer-price-index-ex-food-energy-yy/export",
+    "https://www.mql5.com/en/economic-calendar/european-union/consumer-price-index-yy/export",
+    "https://www.mql5.com/en/economic-calendar/european-union/producer-price-index-mm/export",
+    "https://www.mql5.com/en/economic-calendar/european-union/producer-price-index-yy/export",
+    "https://www.mql5.com/en/economic-calendar/european-union/business-climate-indicator/export",
+    "https://www.mql5.com/en/economic-calendar/european-union/industry-selling-price-expectations/export",
 ]
 
 #Japan
@@ -40,8 +76,33 @@ JP = [
     "https://www.mql5.com/en/economic-calendar/japan/unemployment-rate/export",
     "https://www.mql5.com/en/economic-calendar/japan/nikkei-manufacturing-pmi/export",
     "https://www.mql5.com/en/economic-calendar/japan/nikkei-services-pmi/export",
+    #Single Use Indicators
     "https://www.mql5.com/en/economic-calendar/japan/boj-interest-rate-decision/export",
     "https://www.mql5.com/en/economic-calendar/japan/cftc-jpy-non-commercial-net-positions/export",
+    #NEWEST ADDED
+    "https://www.mql5.com/en/economic-calendar/japan/gross-domestic-product-yy/export",
+    "https://www.mql5.com/en/economic-calendar/japan/labor-cash-earnings-yy/export",
+    "https://www.mql5.com/en/economic-calendar/japan/overtime-pay-yy/export",
+    "https://www.mql5.com/en/economic-calendar/japan/real-wage-yy/export",
+    "https://www.mql5.com/en/economic-calendar/japan/domestic-corporate-goods-price-index-mm/export",
+    "https://www.mql5.com/en/economic-calendar/japan/domestic-corporate-goods-price-index-yy/export",
+    "https://www.mql5.com/en/economic-calendar/japan/national-consumer-price-index-ex-fresh-food-yy/export",
+    "https://www.mql5.com/en/economic-calendar/japan/national-consumer-price-index-ex-food-energy-yy/export",
+    "https://www.mql5.com/en/economic-calendar/japan/economy-watchers-current-index/export",
+    "https://www.mql5.com/en/economic-calendar/japan/economy-watchers-future-index/export",
+    "https://www.mql5.com/en/economic-calendar/japan/tankan-large-all-industry-capex/export",
+    "https://www.mql5.com/en/economic-calendar/japan/coincident-index/export",
+    "https://www.mql5.com/en/economic-calendar/japan/tankan-large-manufacturing-outlook/export",
+    "https://www.mql5.com/en/economic-calendar/japan/tankan-large-non-manufacturing-index/export",
+    "https://www.mql5.com/en/economic-calendar/japan/tankan-large-manufacturing-index/export",
+    "https://www.mql5.com/en/economic-calendar/japan/industrial-production-yy/export",
+    "https://www.mql5.com/en/economic-calendar/japan/leading-index/export",
+    "https://www.mql5.com/en/economic-calendar/japan/consumer-confidence-index/export",
+    "https://www.mql5.com/en/economic-calendar/japan/household-spending-yy/export",
+    "https://www.mql5.com/en/economic-calendar/japan/large-retailers-sales-yy/export",
+    "https://www.mql5.com/en/economic-calendar/japan/retail-sales-yy/export",
+    "https://www.mql5.com/en/economic-calendar/japan/annualized-housing-starts/export",
+    "https://www.mql5.com/en/economic-calendar/japan/housing-starts-yy/export",
 ]
 
 #United Kingdom
@@ -50,8 +111,33 @@ UK = [
     "https://www.mql5.com/en/economic-calendar/united-kingdom/unemployment-rate/export",
     "https://www.mql5.com/en/economic-calendar/united-kingdom/markit-manufacturing-pmi/export",
     "https://www.mql5.com/en/economic-calendar/united-kingdom/markit-services-pmi/export",
+    #Single Use Indicators
     "https://www.mql5.com/en/economic-calendar/united-kingdom/boe-interest-rate-decision/export",
     "https://www.mql5.com/en/economic-calendar/united-kingdom/cftc-gbp-non-commercial-net-positions/export", 
+    #NEWEST ADDED
+    "https://www.mql5.com/en/economic-calendar/united-kingdom/gdp-yy/export",
+    "https://www.mql5.com/en/economic-calendar/united-kingdom/niesr-gdp-estimate/export",
+    "https://www.mql5.com/en/economic-calendar/united-kingdom/average-weekly-earnings-regular-pay/export",
+    "https://www.mql5.com/en/economic-calendar/united-kingdom/average-weekly-earnings-total-pay/export",
+    "https://www.mql5.com/en/economic-calendar/united-kingdom/claimant-count-change/export", #INVERSE!!
+    "https://www.mql5.com/en/economic-calendar/united-kingdom/core-cpi-yy/export",
+    "https://www.mql5.com/en/economic-calendar/united-kingdom/cpi-yy/export",
+    "https://www.mql5.com/en/economic-calendar/united-kingdom/ppi-output-yy/export",
+    "https://www.mql5.com/en/economic-calendar/united-kingdom/rpi-yy/export",
+    "https://www.mql5.com/en/economic-calendar/united-kingdom/business-investment-yy/export",
+    "https://www.mql5.com/en/economic-calendar/united-kingdom/construction-output-yy/export",
+    "https://www.mql5.com/en/economic-calendar/united-kingdom/industrial-production-yy/export",
+    "https://www.mql5.com/en/economic-calendar/united-kingdom/manufacturing-production-yy/export",
+    "https://www.mql5.com/en/economic-calendar/united-kingdom/markit-construction-pmi/export",
+    "https://www.mql5.com/en/economic-calendar/united-kingdom/core-retail-sales-yy/export",
+    "https://www.mql5.com/en/economic-calendar/united-kingdom/gfk-consumer-confidence/export",
+    "https://www.mql5.com/en/economic-calendar/united-kingdom/index-of-services/export",
+    "https://www.mql5.com/en/economic-calendar/united-kingdom/retail-sales-yy/export",
+    "https://www.mql5.com/en/economic-calendar/united-kingdom/boe-mortgage-approvals/export",
+    "https://www.mql5.com/en/economic-calendar/united-kingdom/halifax-house-price-index-yy/export",
+    "https://www.mql5.com/en/economic-calendar/united-kingdom/house-price-index-yy/export",
+    "https://www.mql5.com/en/economic-calendar/united-kingdom/rics-house-price-balance/export",
+    "https://www.mql5.com/en/economic-calendar/united-kingdom/bba-mortgage-approvals/export",
 ]
 
 #Canada
@@ -60,8 +146,17 @@ CA = [
     "https://www.mql5.com/en/economic-calendar/canada/unemployment-rate/export",
     "https://www.mql5.com/en/economic-calendar/canada/markit-manufacturing-pmi/export",
     "https://www.mql5.com/en/economic-calendar/canada/ivey-pmi/export",
+    #Single Use Indicators
     "https://www.mql5.com/en/economic-calendar/canada/boc-interest-rate-decision/export",
-    "https://www.mql5.com/en/economic-calendar/canada/cftc-cad-non-commercial-net-positions/export",   
+    "https://www.mql5.com/en/economic-calendar/canada/cftc-cad-non-commercial-net-positions/export",  
+    #NEWEST ADDED
+    "https://www.mql5.com/en/economic-calendar/canada/gdp-yy/export",
+    "https://www.mql5.com/en/economic-calendar/canada/cpi-yy/export",
+    "https://www.mql5.com/en/economic-calendar/canada/ippi-yy/export",
+    "https://www.mql5.com/en/economic-calendar/canada/rmpi-yy/export",
+    "https://www.mql5.com/en/economic-calendar/canada/capacity-utilization-rate/export",
+    "https://www.mql5.com/en/economic-calendar/canada/ivey-pmi-nsa/export",
+    "https://www.mql5.com/en/economic-calendar/canada/cmhc-housing-starts/export",
 ]
 
 #Australia
@@ -124,14 +219,14 @@ FR = [
 
 Countries = [
     US,
-    EU,
-    JP,
-    UK,
-    CA,
-    AU,
-    CH,
-    NZ,
-    CN,
+    #EU,
+    #JP,
+    #UK,
+    #CA,
+    #AU,
+    #CH,
+    #NZ,
+    #CN,
 ]
 
 
